ee8aebebcfb8c118f0ab22ecb42261e4c354b807
Amrita <amritaaauji228@gmail.com>
2026-03-20 20:48:50 +0530

    Final frontend login integration

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import LoginForm from "./features/authentication/components/LoginForm.jsx";
+
+function App() {
+  return (
+    <div>
+      <h1>Car Rental System</h1>
+      <LoginForm />
+    </div>
+  );
+}
+
+export default App;
